878d345668f4f4b1bda1e33eb09d1903cf981b1e
LuizOtavioPolido <polido.luiz@outlook.com>
2023-04-25 22:55:37 -0300

    atv06

---
+++ Atv06/Atv06.java
@@ -0,0 +1,23 @@
+package Atv06;
+
+public class Atv06 {
+    public static void main(String[] args) {
+        float depositoFM = 1500;
+        float montante = 2000;
+
+        System.out.println("total de meses: " + calcularMeses(depositoFM, montante));
+    };
+
+    public static int calcularMeses(float depositoFM, float montante) {
+        float saldo = 0;
+        int meses = 0;
+        float saldoatual = 0;
+        while (saldoatual < montante) {
+            saldo += depositoFM;
+            saldo *= 0.5;
+            saldoatual += saldo; 
+            meses++;
+        }
+        return meses;
+    };
+}
--- README.md
+++
@@ -1,8 +0,0 @@
-# Sobre:
-Repositório que será utilizado nas aulas de Programação Orientada a Objeto - IFPR
-
-# Aluno: 
-Nome: Luiz Otavio Anderzen Polido <br />
-Email de contato: polido.luiz1@gmail.com
-
-https://mybinder.org/v2/gh/LuizOtavioPolido/JAVA/HEAD
